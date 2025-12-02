d5b9fe9c152cf3e9291a1c8fbf2c9ed3d2a37a94
MightyDisciple <nicolasvandyck@proton.me>
2025-12-02 20:39:58 +0100

    Initialize

---
+++ src/main/java/com/bookreader/bookreader/BookreaderApplication.java
@@ -0,0 +1,13 @@
+package com.bookreader.bookreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookreaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookreader/bookreader/BookreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookreader.bookreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookreaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
